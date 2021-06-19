ef224236d9a6dbf78667da53b98fcf002f1891dd
unknown <terencetan@hotmail.ca>
2021-06-19 01:09:14 -0400

    added cards to the carousel

--- js/init.js
+++ js/init.js
@@ -1,22 +1,25 @@
-(function($){
-  $(function(){
+(function ($) {
+  $(function () {
     // Age verification pop-up
-    $(document).ready(function(){
-      if (sessionStorage.getItem('ageVerified') !== 'true') {
-        $('#age-verification').modal({dismissible: false}).modal('open').blur();
+    $(document).ready(function () {
+      if (sessionStorage.getItem("ageVerified") !== "true") {
+        $("#age-verification")
+          .modal({ dismissible: false })
+          .modal("open")
+          .blur();
       }
     });
 
     // Age verifid, store session
-    $('#age-verification-enter').on('click', function() {
-      sessionStorage.setItem('ageVerified','true');
-    })
+    $("#age-verification-enter").on("click", function () {
+      sessionStorage.setItem("ageVerified", "true");
+    });
 
-    $('.sidenav').sidenav();
-    $('.parallax').parallax();
-    $('.carousel').carousel();
-
+    $(".sidenav").sidenav();
+    $(".parallax").parallax();
+    $(".carousel").carousel({
+      fullWidth: true,
+      indicators: true,
+    });
   }); // end of document ready
 })(jQuery); // end of jQuery name space
-
-
